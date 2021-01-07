2f5bfce439af139538c38f53c4fea70996714742
Михаил Гусев <gusev-miha2011@mail.ru>
2021-01-08 01:37:27 +1000

    Add request friends of friends Changed logic of function "childrens"

--- UsersAPI/UsersAPI.py
+++ UsersAPI/UsersAPI.py
@@ -2,10 +2,12 @@ import requests
 import time
 
 def gui():
-
+    
     key_list = ["id", "first_name", "last_name", "city"]
     name_key = ["id - ", "имя - ", "фамилия - ", "город - "]
     user_list=[]
+    subchildrens=[]
+    countNames = 1
 
     print("Введите идентификатор пользователя")
     id = input()
@@ -14,18 +16,12 @@ def gui():
         gui()
         return
     id = parent(id, key_list, name_key, user_list)
-    childrens(id, key_list, name_key, user_list)
+    childrens(key_list, name_key, user_list, countNames)
+    countNames+=1
+    childrens(key_list, name_key, user_list, countNames)
 
-    with open("users.txt", "w", encoding='utf-8') as file:
-        count=1
-        for line in user_list:
-            row=f"{count} "
-            count+=1
-            for value in range(len(line)):
-                row += str(line[value])
-                if (value+1)%2 == 0:
-                    row+="; "
-            file.writelines(f"{row}\n")
+
+
     
     
     
@@ -33,7 +29,10 @@ def gui():
 def parent(id, key_list, name_key, user_list):
     requestStringUser = f"https://api.vk.com/method/users.get?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_ids={id}&fields=city"
     response = requests.get(requestStringUser)
-    errors(response)
+    if errors(response) == True:
+        print("Нет такой страницы")
+        gui()
+        return
     
     user = response.json()['response']
     index = response.json()['response'][0]['id']
@@ -51,39 +50,62 @@ def parent(id, key_list, name_key, user_list):
         else:
             user_list[0].append(user[0][key_list[key]])
 
+    with open("users.txt", "w", encoding='utf-8') as file:
+        count=1
+        for line in user_list:
+            row=f"0 "
+            count+=1
+            for value in range(len(line)):
+                row += str(line[value])
+                if (value+1)%2 == 0:
+                    row+=" ; "
+            file.writelines(f"{row}\n")
+
     return index
 
-def childrens(id, key_list, name_key, user_list):
-    requestStringFriends = f"https://api.vk.com/method/users.getFollowers?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}"
-    response = requests.get(requestStringFriends)
-    errors(response)
+def childrens(key_list, name_key, user_list, countNames):
+    with open("users.txt", "r", encoding='utf-8') as file:
+        contents = file.readlines()
+        for line in range(len(contents)):
+            splitLine = contents[line].split()
+            id = int(splitLine[3])          
 
-    size = response.json()['response']['count']
-    users=[]
-    offset=0
-    while offset < size:
+            requestStringFriends = f"https://api.vk.com/method/users.getFollowers?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}"
+            response = requests.get(requestStringFriends)
+            if errors(response) == True:
+                with open("users.txt", "a", encoding='utf-8') as fileWrite:
+                    fileWrite.writelines(f"{countNames}.{line}: id - {id} : private \n")
+                continue
 
-        response = requests.get(f"https://api.vk.com/method/users.getFollowers?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}&count=1000&offset={offset}&fields=city")
-        data = response.json()['response']['items']
-        offset+=1000
-        print(offset)
-        users.extend(data)
-        time.sleep(0.1)
+            size = response.json()['response']['count']
+            users=[]
+            offset=0
+            while offset < size:
+                response = requests.get(f"https://api.vk.com/method/users.getFollowers?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}&count=1000&offset={offset}&fields=city")
+                data = response.json()['response']['items']
+                offset+=1000
+                print(offset)
+                users.extend(data)
+                time.sleep(0.1)
 
-    for user in range(len(users)):
-        user_list.append([])
-        for key in range(len(key_list)):
-            user_list[user+1].append(name_key[key])
-            if key == len(key_list)-1:
-                try:
-                    user_list[user+1].append(users[user][key_list[key]]['title'])
-                except:
-                    user_list[user+1].append("Неизвестен")
-            else:
-                try:
-                    user_list[user+1].append(users[user][key_list[key]])
-                except:
-                    user_list[user+1].append("Неизвестен")
+            with open("users.txt", "a", encoding='utf-8') as fileWrite:
+                for user in range(len(users)):
+                    user_list = []
+                    user_list.append(f"{countNames}.{line}.{user}: ")
+                    for key in range(len(key_list)):
+                        user_list.append(name_key[key])
+                        if key == len(key_list)-1:
+                            try:
+                                user_list.append(str(users[user][key_list[key]]['title']) + " ; ")
+                            except:
+                                user_list.append("Неизвестен ; ")
+                        else:
+                            try:
+                                user_list.append(str(users[user][key_list[key]]) + " ; ")
+                            except:
+                                user_list.append("Неизвестен ; ")
+                    user_line = ''.join(user_list)
+                    fileWrite.writelines(f"{user_line}\n")
 
 def errors(response):
     if response.status_code != 200:
@@ -94,8 +116,8 @@ def errors(response):
         response.json()['response']
     except:
         print(response.json()['error']['error_msg'])
-        gui()
-        return
+        #gui()
+        return True
     if len(response.json()['response']) < 1:
         print("Ошибка запроса")
         gui()
