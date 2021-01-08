425083b1d4ee7e1103582f13167591e291218c12
Михаил Гусев <gusev-miha2011@mail.ru>
2021-01-08 21:20:19 +1000

    change something

--- UsersAPI/UsersAPI.py
+++ UsersAPI/UsersAPI.py
@@ -19,12 +19,7 @@ def gui():
     childrens(key_list, name_key, user_list, countNames)
     countNames+=1
     childrens(key_list, name_key, user_list, countNames)
-
-
-
-    
-    
-    
+        
 
 def parent(id, key_list, name_key, user_list):
     requestStringUser = f"https://api.vk.com/method/users.get?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_ids={id}&fields=city"
@@ -70,7 +65,7 @@ def childrens(key_list, name_key, user_list, countNames):
             splitLine = contents[line].split()
             id = int(splitLine[3])          
 
-            requestStringFriends = f"https://api.vk.com/method/users.getFollowers?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}"
+            requestStringFriends = f"https://api.vk.com/method/friends.get?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}"
             response = requests.get(requestStringFriends)
             if errors(response) == True:
                 with open("users.txt", "a", encoding='utf-8') as fileWrite:
@@ -81,7 +76,7 @@ def childrens(key_list, name_key, user_list, countNames):
             users=[]
             offset=0
             while offset < size:
-                response = requests.get(f"https://api.vk.com/method/users.getFollowers?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}&count=1000&offset={offset}&fields=city")
+                response = requests.get(f"https://api.vk.com/method/friends.get?access_token=3c10b57e3c10b57e3c10b57ef33c645bb533c103c10b57e63a01af78fd985745dfee641&v=5.126&user_id={id}&count=1000&offset={offset}&fields=city")
                 data = response.json()['response']['items']
                 offset+=1000
                 print(offset)
